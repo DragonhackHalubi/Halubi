e90d7255a9bbcdc30de459516555fabe89ebcca8
Gal petkovšek <gp1914@student.uni-lj.si>
2023-05-13 14:44:39 +0200

    Working trip generator & test

--- src/utils/chatgpt_call.py
+++ src/utils/chatgpt_call.py
@@ -1,7 +1,7 @@
 import requests
 from typing import Dict, List
 
-def chatgpt_call(prompt: str, token: str, model: str = "gpt-3.5-turbo"):
+def _chatgpt_call(prompt: str, token: str, model: str = "gpt-3.5-turbo"):
     response = requests.post(
         "https://openai-api.meetings.bio/api/openai/chat/completions",
         headers={"Authorization": f"Bearer {token}"},
@@ -13,11 +13,26 @@ def chatgpt_call(prompt: str, token: str, model: str = "gpt-3.5-turbo"):
     breakpoint()
     if response.ok:
         content = response.json()["choices"][0]["message"]["content"]
+        print(len(content.split()))
         print(content)
         return content
 
-def generate_trip(plan: Dict[str, str], preferences: List[str], token: str, model: str = "gpt-3.5-turbo"):
-    prompt = """
+def generate_trip(plan: Dict[str, str], preferences: List[str], token: str,
+                  model: str = "gpt-3.5-turbo"):
+    prompt = f"""
+    Plan a trip with the information provided the text delimited by triple backticks. The 
+    information will be in json structure where the keys are gonna be dates or range of dates 
+    and the value will be the location where we are gonna be that day. The output should be in 
+    json format where the keys are dates and the the values are up to three suggestions for that 
+    day where each suggestion is one sentence long. You will also be provided with a list of 
+    interests which can be taken into account when choosing activities. The interests are listed 
+    inside <> list interests in response. Do not include backticks in response.
+
+    ```{str(plan)}```
+
+    <{", ".join(preferences)}>
     """
 
-    return chatgpt_call(prompt, token, model)
+    print(len(prompt.split()))
+
+    return _chatgpt_call(prompt, token, model)
--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 from dotenv import load_dotenv
 import os
+import json
 
-from src.utils.chatgpt_call import chatgpt_call
+from src.utils.chatgpt_call import generate_trip
 
 prompt = "Write Haiku about Dragonhack"
 
@@ -9,4 +10,20 @@ load_dotenv()
 
 token = os.getenv("CHATGPT_KEY")
 
-chatgpt_call(prompt, token)
+#chatgpt_call(prompt, token)
+
+plan = {
+    "19.2.-20.2.": "London",
+    "21.2.": "Rome"
+}
+
+preferences = ["museums", "theater", "sport", "nature"]
+
+resp = generate_trip(plan, preferences, token)
+breakpoint()
+
+try:
+    r_dict = json.loads(resp.replace("`", ""))
+    print(r_dict)
+except:
+    pass
