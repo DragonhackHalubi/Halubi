f9ff647bc0750b19f6c107418908dae63798db83
Gal petkovšek <gp1914@student.uni-lj.si>
2023-05-13 16:07:56 +0200

    citz api

--- test.py
+++ test.py
@@ -3,16 +3,20 @@ import os
 import json
 
 from src.utils.chatgpt_call import generate_trip
+from src.utils.city_api import get_city_coordinates
 
 prompt = "Write Haiku about Dragonhack"
 
 load_dotenv()
 
 token = os.getenv("CHATGPT_KEY")
+city_token = os.getenv("CITY_API_TOKEN")
 
 #chatgpt_call(prompt, token)
 
-plan = {
+coords = get_city_coordinates("Ljubljana", city_token)
+
+"""plan = {
     "19.2.-20.2.": "London",
     "21.2.": "Rome"
 }
@@ -26,4 +30,5 @@ try:
     r_dict = json.loads(resp.replace("`", ""))
     print(r_dict)
 except:
-    pass
+    pass"""
+
